500382247c981acada8c7450e5f00ea1a2b0377a
Typhoonz0 <coolchild1230@gmail.com>
2025-05-12 09:23:48 +0000

    Update server.py

--- server.py
+++ server.py
@@ -10,8 +10,12 @@ DB_FILE = "package_db.json"
 def load_db():
     if not os.path.exists(DB_FILE):
         return {}
-    with open(DB_FILE, "r") as f:
-        return json.load(f)
+    try:
+        with open(DB_FILE, "r") as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return {}
+
 
 def save_db(db):
     with open(DB_FILE, "w") as f:
